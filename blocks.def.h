064414af7b77e54c721aaa7beb8d65bf134dd1a7
Deepak Patel <dp@codex>
2024-07-13 08:47:15 +0530

    Testing

--- blocks.def.h
+++ blocks.def.h
@@ -1,11 +1,15 @@
-//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+// Modify this file to change what commands output to your statusbar, and
+// recompile using the make command.
 static const Block blocks[] = {
-	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
-
-	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
+    /*Icon*/ /*Command*/ /*Update Interval*/ /*Update Signal*/
+    {" Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g", 30, 0},
+    {"BATT: ", "BatteryNotify", 30, 0},
+    {"MON: ", "getMon", 0, 10},
+    {"VOL: ", "getvol", 0, 11},
+    {"", "date '+%b %d (%a) %I:%M%p'", 20, 0}
 };
 
-//sets delimeter between status commands. NULL character ('\0') means no delimeter.
+// sets delimeter between status commands. NULL character ('\0') means no
+// delimeter.
 static char delim[] = " | ";
 static unsigned int delimLen = 5;
